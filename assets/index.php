efad9aee01be61076d8ea0023df0cf39df769910
Suru <Surhit@github.com>
2020-05-23 09:10:26 +0530

    First commit

---
+++ assets/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+		<html>
+			<head>
+				<meta name="viewport" content="width=device-width">
+				<link id="favicon" rel="shortcut icon" href="TD.ico" type="image/ico">
+				<script type="text/javascript" src="js/jquery.js"></script>
+				<link rel="stylesheet" type="text/css" href="css/todos.css">
+				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+				<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+				
+				<title><?php echo'Todo List jQuery' ?></title>
+			</head>
+			<body>
+				<div id="container">
+					<h1>To-Do List <span id="toggle-form"><i class="fa fa-plus"></i></span></h1>
+					<input type="text" placeholder="Add New Todo">
+
+					<ul>
+
+					</ul>
+				</div>
+
+				<script type="text/javascript" src="js/todos.js"></script>
+			</body>
+		</html>
